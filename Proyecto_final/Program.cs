b681dfb9b482bcf5354ce8520bd57393ca4ba522
Estudiante <estudiante@universidad.edu.sv>
2026-05-25 15:36:27 -0600

    feat: crear punto de entrada Program.cs y configurar servicios

---
+++ Proyecto_final/Program.cs
@@ -0,0 +1,12 @@
+﻿using Proyecto_final.Services;
+using Proyecto_final.UI;
+using Proyecto_final.Data;
+
+// Inicializar servicios
+var libroService = new LibroService();
+var usuarioService = new UsuarioService();
+var prestamoService = new PrestamoService(libroService, usuarioService);
+
+// Crear y mostrar menú
+var menu = new Menu(libroService, usuarioService, prestamoService);
+menu.MostrarMenuPrincipal();
